07205fe953dbe85f75946daf5ef8fca9649deb7c
zajackarolina <147318667+zajackarolina@users.noreply.github.com>
2024-01-11 18:13:31 +0100

    commit

--- scripts/file.js
+++
@@ -1,9 +0,0 @@
-const input = document.getElementById('photo');
-
-input.addEventListener('change', function () {
-    if (input.files.length > 0) {
-        input.nextElementSibling.textContent = input.files[0].name;
-    } else {
-        input.nextElementSibling.textContent = 'Wybierz plik';
-    }
-});
--- scripts/quotes.js
+++ scripts/quotes.js
@@ -1,16 +1,16 @@
 const quotes = [
-    "Pies je. Kot jada.",
-    "Na myszy zawsze łasy kot.",
-    "Psy mają swoich panów, koty – służących",
-    "Kot działa według zasady: proś, a może Ci dam.",
+  "Pies je. Kot jada.",
+  "Na myszy zawsze łasy kot.",
+  "Psy mają swoich panów, koty – służących",
+  "Kot działa według zasady: proś, a może Ci dam.",
   "Kot ma zbyt wielką duszę, by nie mieć serca.",
-    "Koty są istotami tajemniczymi, w ich umysłach jest więcej niż możemy sobie wyobrazić.",
-"Kotom bez trudu udaje się to, co nie jest dane człowiekowi: iść przez życie nie czyniąc hałasu"
+  " Wiecie jak to jest z kotami: nie mają właścicieli, tylko służbę",
+  "Baba jak ten kot: kto pogłaszcze, ten panem",
+  "Wszystko, co dobre, zaczyna się od kota."
 ];
 
-function changeQuote() 
-{
-    const quoteElement = document.getElementById("quote");
-    const randomIndex = Math.floor(Math.random() * quotes.length);
-    quoteElement.innerText = quotes[randomIndex];
+function changeQuote() {
+  const quoteElement = document.getElementById("quote");
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  quoteElement.innerText = quotes[randomIndex];
 }
